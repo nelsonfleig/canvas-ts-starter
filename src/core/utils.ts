21f8f8b7cf3cea701e51d7d4716fd37d60ee696f
Nelson Fleig <nelsonfleig@Nelsons-MacBook-Pro.local>
2023-12-29 19:36:52 +0100

    add map function

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -13,3 +13,17 @@ export const degToRad = (degrees: number) => {
 export const radToDeg = (radians: number) => {
   return radians * (180 / Math.PI);
 };
+
+export const map = (value: number, min1: number, max1: number, min2: number, max2: number) => {
+  if (min1 > max1) {
+    const temp = min1;
+    min1 = max1;
+    max1 = temp;
+  }
+  if (min2 > max2) {
+    const temp = min2;
+    min2 = max2;
+    max2 = temp;
+  }
+  return min2 + (max2 - min2) * ((value - min1) / (max1 - min1));
+};
